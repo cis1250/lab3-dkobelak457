968a3ae228de13a15275fbdd744ea1a8ce3493e0
dkobelak457 <davidkobelak@gmail.com>
2025-09-29 13:29:05 -0400

    Update word_frequency.py

--- word_frequency.py
+++ word_frequency.py
@@ -37,35 +37,35 @@ while is_sentence(user_sentence) == False:
 
 #converts all the words to lowercase
 lower_sentence = user_sentence.lower()
-#re.sub is used to clean the sentences, ex replace capitals ect
-cleaned_sentence = re.sub(r'[^\w\s]', ' ', lower_sentence)
+#used gemeni AI for this portion
+#the cleaned_sentence works 
+cleaned_sentence = re.sub(r'[^\w\s]',' ', lower_sentence)
+
 #split is used to split the sentences up into indivudual strings
 all_words = cleaned_sentence.split()
-
 #Empty strings for words and frequency 
 unique_words = []
 frequencies = []
 
 #for loop to get words and add them to a list 
-# word is temporary, used to see if words are inside of the list
+# i is temporary, used to see if words are inside of the list
+word = 0
 for word in all_words:
-    if word: # Ensures only process non empty strings
-        if word in unique_words:
-            # If the word is already in the unique list, increment its count 
-            index = unique_words.index(word)
-            frequencies[index] += 1
-        else:
-            # If it's a new word, add it to the unique list and set its initial frequency to 1
-            unique_words.append(word)
-            frequencies.append(1)
-
+     if word in unique_words:
+        # If the word is already in the unique list, increment its count 
+        i = unique_words.index(word)
+        frequencies[i] += 1
+     else:
+        # If it's a new word, add it to the unique list and set its initial frequency to 1
+        unique_words.append(word)
+        frequencies.append(1)
 
 print("\n Word Frequency Results ")
 # Only print results if words were actually found
+
+# Took this from Gemeni AI
 if unique_words:
-# zip both unique words and frequencies for easy clean access 
-    
-    for word, count in unique_words, frequencies:
-        print(f"'{word}': {count}")
+    for word, count in zip(unique_words, frequencies):
+        print(f"{word} : {count}")
 else:
     print("No words found ") 
