ffd32344fa1b654dc2c4053834dd3d137f2f26ee
dkobelak457 <davidkobelak@gmail.com>
2025-10-20 11:53:35 -0400

    Refactor Fibonacci sequence logic into functions
    Refactor Fibonacci sequence code to use functions for user input and sequence generation.

--- fibonacci.py
+++ fibonacci.py
@@ -7,8 +7,9 @@
 # Use a for loop to print the Fibonacci sequence up to that many terms.
 
 #While loop takes the user input and checks if it is a valid integer
-while True:
-    n = input("Enter how many Fibonacci Sequence integers do you want: ")
+
+def userInput():
+        n = input("Enter how many Fibonacci Sequence integers do you want: ")
     #checks if the input is only digits
     if n.isdigit():
         n = int(n)
@@ -19,13 +20,18 @@ while True:
             print("Choose a positive integer ")
     else:
         print("Invalid, enter a whole number ")
-a = 0
-b = 1
-#for loop takes a then calculates the next fib num using the t as a temp variable
-#saves the old value of a, updates a and b to move the sequence forwards
-for i in range(n):
-    print(a, end=" ")
-    t = a
-    a = b
-    b = t + b
-print()
+def fibonacci():
+    a = 0
+    b = 1
+    #for loop takes a then calculates the next fib num using the t as a temp variable
+    #saves the old value of a, updates a and b to move the sequence forwards
+    for i in range(n):
+        print(a, end=" ")
+        t = a
+        a = b
+        b = t + b
+    print()
+    
+while True:
+    userInput()
+fibonacci()
